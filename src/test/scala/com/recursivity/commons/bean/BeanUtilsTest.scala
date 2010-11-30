afc8d1c7e481867eb34a60c98805e71a777a4b99
Wille Faler <wille.faler@gmail.com>
2010-11-30 01:25:24 +0000

    added Named annotation, renamed class

---
+++ src/test/scala/com/recursivity/commons/bean/BeanUtilsTest.scala
@@ -0,0 +1,24 @@
+package com.recursivity.commons.bean
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wfaler
+ * Date: Nov 29, 2010
+ * Time: 11:52:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class BeanUtilsTest extends FunSuite{
+
+  test("instantiate case-class"){
+    val bean = BeanUtils.instantiate(classOf[TestBean]).asInstanceOf[TestBean]
+    assert(bean != null)
+    assert(bean.isInstanceOf[TestBean])
+    assert(bean.option == None)
+  }
+}
+
+case class TestBean(name: String, long: Long, int: Int, short: Short, double: Double, float: Float,
+                    bool: Boolean, b: Byte, char: Char, decimal: BigDecimal, option: Option[String], list: List[String])
