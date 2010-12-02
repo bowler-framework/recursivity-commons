b6aa3ba839b54f6e8d8144d5bb056a1ff34423b8
Wille Faler <wille.faler@gmail.com>
2010-12-02 18:18:30 +0000

    changed if-else to pattern matching

--- src/main/scala/com/recursivity/commons/bean/BeanUtils.scala
+++ src/main/scala/com/recursivity/commons/bean/BeanUtils.scala
@@ -15,36 +15,38 @@ object BeanUtils{
   def instantiate(cls: Class[_]): Any ={
     val cons = cls.getConstructors.head
     val list = new MutableList[AnyRef]
-    cons.getParameterTypes.foreach(cls =>{
-      if(cls.getName.equals("long")){
-        val l : Long = 0
-        list += l.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("int")){
-        val l : Int = 0
-        list += l.asInstanceOf[AnyRef]        
-      }else if(cls.getName.equals("float")){
-        val f = 0.0f
-        list += f.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("double")){
-        val f = 0.0d
-        list += f.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("boolean")){
-        val b = false
-        list += b.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("short")){
-        val l : Short = 0
-        list += l.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("byte")){
-        val b = new java.lang.Byte("0")
-        list += b.asInstanceOf[AnyRef]
-      }else if(cls.getName.equals("char")){
-        val c = new java.lang.Character('c')
-        list += c
-      }else if(cls.getName.equals("scala.Option")){
-        list += None
-      }else list += null
-    })
+      cons.getParameterTypes.foreach(cls =>{
+        cls.getName match{
+          case "long" => {
+            val l : Long = 0
+            list += l.asInstanceOf[AnyRef]
+          }case "int" => {
+            val l : Int = 0
+            list += l.asInstanceOf[AnyRef]
+          }case "float" => {
+            val f = 0.0f
+            list += f.asInstanceOf[AnyRef]
+          }case "double" => {
+            val f = 0.0d
+            list += f.asInstanceOf[AnyRef]
+          }case "boolean" => {
+            val b = false
+            list += b.asInstanceOf[AnyRef]
+          }case "short" => {
+            val l : Short = 0
+            list += l.asInstanceOf[AnyRef]
+          }case "byte" => {
+            val b = new java.lang.Byte("0")
+            list += b.asInstanceOf[AnyRef]
+          }case "char" => {
+            val c = new java.lang.Character('c')
+            list += c
+          }case "scala.Option" => list += None
+          case _ => list += null
 
-    return cons.newInstance(list.toArray:_*)    
+        }
+    })
+    println(list.size)
+    return cons.newInstance(list.toArray:_*) 
   }
 }
